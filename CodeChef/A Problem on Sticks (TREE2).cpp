587eadb7ad2443ed394dcfc9a0cbc3baa16ce419
Rafat <rafathaque1997@gmail.com>
2020-09-27 15:15:11 +0600

    :+1:  Added code

---
+++ CodeChef/A Problem on Sticks (TREE2).cpp
@@ -0,0 +1,55 @@
+/**
+Not Solved
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ll long long
+#define fastoi  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define printNPrecision(afterDigit,value) cout << fixed << setprecision(12) << value <<endl;
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+
+
+#define LSOne(S) (S & (-S))
+#define isBitSet(S, i) ((S >> i) & 1)
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+
+void solve(){
+    int n,val,ans = 0;
+    vector<int> data;
+    cin>>n;
+    for(int i = 0 ; i < n ; i++){
+        cin>>val;
+        data.push_back(val);
+    }
+    sort(data.begin(), data.end());
+    ans = data[0];
+    for(int i = 0 ; i < data.size();i++){
+        ans += (data[i] - ans);
+    }
+    cout<<ans<<endl;
+}
+
+
+
+int main()
+{
+    fastoi ;
+    int t;
+    cin>>t;
+    while(t--)
+        solve();
+
+    return 0;
+}
